de5c7a0ec3ff8b5a24d3b7b47f875639891d1108
ScottLindley <scottdlindley@gmail.com>
2023-03-11 09:16:02 -0500

    Set up react client with vite

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import { getAllNotes } from './notes';
 
 const notesRouter = express.Router();
@@ -14,5 +15,13 @@ apiRouter.use('/notes', notesRouter);
 const app = express();
 
 app.use('/api', apiRouter);
+
+// Serve app production bundle
+app.use(express.static('dist/client'));
+
+// Handle client routing, return all requests to the app
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'client/index.html'));
+});
 
 app.listen(process.env.PORT || 8080, () => console.log('🚀'));
