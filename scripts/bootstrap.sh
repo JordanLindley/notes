ca4d6afe01355b7c01fae7213478a16373031e36
ScottLindley <scottdlindley@gmail.com>
2023-03-14 18:16:17 -0400

    Use an environment variable when connecting to the DB

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Create an env file from the example env if it doesn't already exist.
+if [ ! -f .env ]; then
+  cp .env.example .env
+fi
+
+# Install dependencies.
+npm install
+
+# Create the "notes" db (will fail if it already exists but that's okay).
+createdb notes
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,16 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import path from 'path';
-import { getAllNotes, createNote, deleteNote, updateNote, readNote } from './notes';
+import {
+  getAllNotes,
+  createNote,
+  deleteNote,
+  updateNote,
+  readNote,
+} from './notes';
 import bodyParser from 'body-parser';
-
 
 const notesRouter = express.Router();
 
@@ -14,25 +22,25 @@ notesRouter.get('/:id', async (req, res) => {
   const id = await req.params.id;
 
   res.send(await readNote(id));
-})
+});
 
 notesRouter.post('/', async (req, res) => {
   const { owner, title, body } = await req.body;
   res.send(await createNote(owner, title, body));
-})
+});
 
 notesRouter.delete('/:id', async (req, res) => {
   const id = await req.params.id;
-  
-  res.send(await deleteNote(id));
-})
 
-notesRouter.patch('/:id',async (req, res) => {
+  res.send(await deleteNote(id));
+});
+
+notesRouter.patch('/:id', async (req, res) => {
   const { title, body } = await req.body;
   const id = await req.params.id;
 
   res.send(await updateNote(id, title, body));
-})
+});
 
 const apiRouter = express.Router();
 
