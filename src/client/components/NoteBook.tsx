6dc78b8302585e9c103ed88af0cda961da7c6c20
JordanLindley <jlindley94@gmail.com>
2023-05-12 22:19:07 -0400

    contain delete button in div, change div to flex, center button

--- src/client/components/NoteBook.tsx
+++ src/client/components/NoteBook.tsx
@@ -21,9 +21,9 @@ export default function NoteBook(props: {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="new-note"
         >
           <line x1="12" y1="5" x2="12" y2="19"></line>
