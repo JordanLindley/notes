5ac2149297097475989d929ce05c8269e53fa914
JordanLindley <jlindley94@gmail.com>
2023-05-21 16:56:21 -0400

    create users migration

--- db/migrations/000002_users.up.sql
+++ db/migrations/000002_users.up.sql
@@ -1,7 +1,9 @@
-
+BEGIN;
 CREATE TABLE users(
   id text PRIMARY KEY DEFAULT gen_random_uuid(),
   email text UNIQUE NOT NULL,
+  digest text UNIQUE NOT NULL,
   created_at timestamp without time zone NOT NULL DEFAULT NOW()
 );
-
+CREATE INDEX users_email_index ON users(email);
+COMMIT;
