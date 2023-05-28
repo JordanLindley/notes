0633fe4d9a4c2f56b9cae01463361bc3af1937de
JordanLindley <jlindley94@gmail.com>
2023-05-28 12:12:07 -0400

    progress on login - not ready to test yet

--- src/server/users/login.ts
+++ src/server/users/login.ts
@@ -4,37 +4,49 @@ import bcrypt from 'bcrypt';
 import { hashPassword } from './signup';
 import { v4 as uuidv4 } from 'uuid';
 
-export async function login(email, pass) {
+export type Session = {
+  token: string;
+  expires_at: object;
+};
+
+export async function login(email: string, pass: string) {
   const client = await getClient();
-  const hashedPass = await hashPassword(pass);
+  const digest = await hashPassword(pass);
+
   // look up email in user db, get user record from query
   const res = await client.query<User>(
-    `SELECT * FROM users WHERE email = $1 RETURNING email`,
+    `SELECT * FROM users WHERE email = $1 RETURNING email, id`,
     [email]
   );
+
   // return err if not found or null
   if (res.rows[0] == null) {
     return 'email not found!';
   }
-  // use bcrypt.compare() to check pw give against digest in db
-  bcrypt.compare(pass, hashedPass, function (err, hash) {
-    if (err) {
-      return `incorrect password!`;
-    }
-    if (hash) {
-      // return json stuff??
-      // proceed if matches
-    } else {
-      // return hash.json({success: false, message: `incorrect password!`}); <- returning an error on .json...
-    }
 
-    // insert a new session record to session table. Create new token, hash that token.
-    // uuidv4() -- creates random uuid.
-    // hash token before issuing. Enter into sessions table for hashed_access_token
-    // const hashedToken = hadPassword(token);
-    // create a new Date() in js for a timeout on session. Reference date 2 weeks from Now as expires_at timestamp, insert into sessions table.
-    // return unhashed access token to user.
-    // token will be hashed and compared to sessions table upon each user request
-    // use middleware for ease of use?
-  });
+  // use bcrypt.compare() to check pw given against digest in db
+  if (await bcrypt.compare(pass, digest)) {
+    //returns true or false
+    console.log('success!');
+    // proceed if matches
+    async function createSession() {
+      // create new token, hash that token.
+      const token = uuidv4();
+      const hashedToken = hashPassword(token);
+      // create a new Date() in js for a timeout on session. Reference date 2 weeks from Now
+      const timeout = new Date(Date.now() + 12096e5);
+
+      // hash token before issuing. Enter into sessions table for hashed_access_token
+      // const hashedToken = hadPassword(token);
+      const newSession = await client.query<Session>(
+        `INSERT INTO sessions (user_id, hashed_access_token, expires_at) VALUES ($1, $2, $3) RETURNING user_id, expires_at`,
+        [hashedToken, timeout] // trouble grabbing user ID here...
+      );
+      // return unhashed access token to user.
+      // token will be hashed and compared to sessions table upon each user request
+    }
+  } else {
+    return 'password incorrect';
+  }
+  // use middleware for ease of use?
 }
