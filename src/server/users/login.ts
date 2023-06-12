13af46767260461aab846c387376924aae51e8dd
JordanLindley <jlindley94@gmail.com>
2023-06-12 12:46:52 -0400

    add digest to login query, insert hashed token into sessions db

--- src/server/users/login.ts
+++ src/server/users/login.ts
@@ -14,8 +14,9 @@ export async function login(email: string, pass: string) {
   const digest = await hashPassword(pass);
 
   // look up email in user db, get user record from query
+  // return digest from query as well as email and userID.
   const res = await client.query<User>(
-    `SELECT * FROM users WHERE email = $1 RETURNING email, id`,
+    `SELECT * FROM users WHERE email = $1 RETURNING email, id, digest`,
     [email]
   );
 
@@ -30,19 +31,19 @@ export async function login(email: string, pass: string) {
     console.log('success!');
     // proceed if matches
     async function createSession() {
-      // create new token, hash that token.
+      // create new token then hash token.
       const token = uuidv4();
       const hashedToken = hashPassword(token);
       // create a new Date() in js for a timeout on session. Reference date 2 weeks from Now
       const timeout = new Date(Date.now() + 12096e5);
 
-      // hash token before issuing. Enter into sessions table for hashed_access_token
-      // const hashedToken = hadPassword(token);
+      // Enter into sessions table for hashed_access_token
       const newSession = await client.query<Session>(
-        `INSERT INTO sessions (user_id, hashed_access_token, expires_at) VALUES ($1, $2, $3) RETURNING user_id, expires_at`,
+        `INSERT INTO sessions (hashed_access_token, expires_at) VALUES ($1, $2) RETURNING user_id, expires_at`,
         [hashedToken, timeout] // trouble grabbing user ID here...
       );
       // return unhashed access token to user.
+
       // token will be hashed and compared to sessions table upon each user request
     }
   } else {
